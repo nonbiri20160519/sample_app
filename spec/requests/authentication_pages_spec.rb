75b47e665153b82789f64f3ebad8f5e01b1033db
nonbiri <nonbiri@mbr.nifty.com>
2017-04-27 13:31:10 +0900

    Finish 6.4

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,6 +13,16 @@ describe "Authentication" do
 	
 	describe "signin" do
 		before { visit signin_path }
+		
+		describe "not signed in" do
+			let(:user) { FactoryGirl.create(:user) }
+			
+			it { should_not have_link('Users', href: users_path) }
+			it { should_not have_link('Profile', href: user_path(user)) }
+			it { should_not have_link('Settings', href: edit_user_path(user)) }
+			it { should_not have_link('Sign out', href: signout_path) }
+			it { should have_link('Sign in', href: signin_path) }
+		end
 
 		describe "with invalid information" do
 			before { click_button "Sign in" }
@@ -51,9 +61,10 @@ describe "Authentication" do
 			describe "when attempting to visit a protected page" do
 				before do
 					visit edit_user_path(user)
-					fill_in "Email", with: user.email
-					fill_in "Password", with: user.password
-					click_button "Sign in"
+					#fill_in "Email", with: user.email
+					#fill_in "Password", with: user.password
+					#click_button "Sign in"
+					sign_in user
 				end
 
 				describe "after signing in" do
@@ -81,6 +92,36 @@ describe "Authentication" do
 					it { should have_title('Sign in') }
 				end
 			end
+
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(user)
+					fill_in "Email", with: user.email
+					fill_in "Password", with: user.password
+					click_button "Sign in"
+				end
+
+				describe "after signing in" do
+
+					it "should render the desired protected page" do
+						expect(page).to have_title('Edit user')
+					end
+					
+					describe "when signing in again" do
+						before do
+							delete signout_path
+							visit signin_path
+							fill_in "Email", with: user.email
+							fill_in "Password", with: user.password
+							click_button "Sign in"
+						end
+
+						it "should render the default(profile) page" do
+							expect(page).to have_title(user.name)
+						end
+					end
+				end
+			end
 		end
 
 		describe "as wrong user" do
@@ -99,6 +140,21 @@ describe "Authentication" do
 				specify { expect(response).to redirect_to(root_path) }
 			end
 		end
+		
+		describe "as singed user" do
+			let(:user) {FactoryGirl.create(:user) }
+			before { sign_in user, no_capybara: true }
+			
+			describe "submitting a GET request to the User#new action" do
+				before { get new_user_path(user) }
+				specify { expect(response).to redirect_to(root_url) }
+			end
+
+			describe "submitting a POST reuquest to the User#create action" do
+				before { post users_path(user) }
+				specify { expect(response).to redirect_to(root_path) }
+			end
+		end
 
 		describe "as non-admin user" do
 			let(:user) { FactoryGirl.create(:user) }
@@ -110,6 +166,16 @@ describe "Authentication" do
 				before { delete user_path(user) }
 				specify { expect(response).to redirect_to(root_path) }
 			end
+		end
+		
+		describe "as admin user" do
+			let(:admin) { FactoryGirl.create(:admin) }
+
+			before { sign_in admin, no_capybara: true }
+			describe "submitting a DELETE request to the Users#destroy action" do
+				before { delete user_path(admin) }
+				specify { expect(response).to redirect_to(root_path) }
+			end			
 		end
 	end
 end
